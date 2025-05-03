b1854487948b3fd1d924acf3c2efbd0d24cd7ad2
Dave7c7 <dave7c7@gmail.com>
2025-05-03 19:13:03 -0400

    final version

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@ let dice = {
   sides: 6,
   roll: function () {
   let randomNumber;
+  
+  
   // Write Code Here
   // Generate a random number between 1 and the number of sides and assign it to randomNumber
-
+    randomNumber = Math.floor((Math.random() * 6));
    return (randomNumber);
   }
 };
-
+const img = [
+  "img/dice-six-faces-five.png",
+  "img/dice-six-faces-four.png",
+  "img/dice-six-faces-one.png",
+  "img/dice-six-faces-six.png",
+  "img/dice-six-faces-three.png",
+  "img/dice-six-faces-two.png"
+]
 function printNumber(number) {
   let placeholder = document.getElementById("placeholder");
-  placeholder.innerHTML = number;
+  placeholder.innerHTML = `<img class="dice" src=${img[number]} alt="ramdom dice image">`;
+
 }
 
 const button = document.getElementById("button");
@@ -19,4 +29,5 @@ const button = document.getElementById("button");
 button.onclick = function() {
   let result = dice.roll();
   printNumber(result);
+  console.log(Number(dice.randomNumber))
 };
